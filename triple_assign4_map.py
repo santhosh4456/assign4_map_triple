30bbc595a6acd45bee1483a5a94c505832923cd7
santhosh4456 <100783089+santhosh4456@users.noreply.github.com>
2022-04-03 10:52:49 +0530

    Create triple_assign4_map.py

---
+++ triple_assign4_map.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+list_of_numbers = [2,5,1,4,8,7]
+out = []
+for i in list_of_numbers:
+    out.append(i**3)
+print(out)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
